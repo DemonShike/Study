a4057577fdd3d8a7aa421a05902c4076db5d1111
DemonShike <103592649+DemonShike@users.noreply.github.com>
2022-04-13 13:29:54 -0300

    Add files via upload

---
+++ comentarios.py
@@ -0,0 +1,23 @@
+# Esto me mostrara el texto hola por la consola 
+print("hola")
+#estas dos intruscciones muestran texto por pantalla
+print("mundo")
+print("!!!")
+
+
+"""
+print("uwu mira soy invisible")
+print("uwu mira soy invisible")
+
+print("uwu mira soy invisible") 
+"""
+
+ #eso de arriba sirve para varias lineas de texto asi no tengo que usar muchos hashtags
+
+print("uwu")
+
+#comandos que uso 
+#dir:lo uso para ver que archivos hay en una carpeta
+#cls:lo uso para borrar el historial del cmd
+#cd:lo uso para elegir en que ubicacion quiero que funcione la consola
+#cd .. :lo uso para ir atras en las carpetas de la consola
---
+++ holamundo.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+
+print("Hola mundo soy demonshike estudiante del curso avanzado de programacion en pyton")
+print("uwu me gusta la corneta 2077punk")
+print("deam boooooy")
+print("12324343")
