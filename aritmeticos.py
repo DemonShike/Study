bad2e12d1e8e78e774d287a8302fd63774e24aae
DemonShike <103592649+DemonShike@users.noreply.github.com>
2022-04-19 12:42:47 -0300

    Add files via upload

---
+++ aritmeticos.py
@@ -0,0 +1,15 @@
+#operadores aritmeticos (nos permiten hacer operaciones con variables y numeros)
+numero1 = 77
+numero2 = 44  #operador de asignacion = 
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2 
+resto = numero1 % numero2 #nos da el resto de los numeros elegidos
+
+print("************** CALCULADORA **************")
+print(f"La Resta es : {resta}")
+print(f"La Suma es : {numero1 + numero2}")
+print("La multiplicacicon es: ", multiplicacion) #esta es otra forma de mostrar el resultado que funciona igual que las demas
+print("La divsion es: ", division) 
+print("El resto de numero 1 y numero 2 es:{}".format(resto))
---
+++ asignacion.py
@@ -0,0 +1,27 @@
+# operadores de asignacion 
+edad = 55
+
+print(edad)
+
+edad += 5
+edad = edad + 5 #esto es lo mismo que el ejercicio de arriba pero menos resumido,su funcion es la misma
+edad -= 5
+edad == 5 #significa, si 5 es igual o igual
+print(edad)
+
+#operadores de incremento y decremento
+year = 2021
+
+# incremento
+year = year + 1
+
+#decremento 
+year = year - 1
+
+'''
+year++     (esto normalmente se usa para sumarle o restarle 1 numero a la variable seleccionada)
+year--    (Esto no sirve en python pero se reemplaza de la forma que esta arriba)
+--year
+++year
+
+'''
